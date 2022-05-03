44fd66b67b7114a080369e5e506413c9e4cb0589
Gabriel Clarke <gabhilto021@student.wethinkcode.co.za>
2022-05-03 14:43:37 +0000

    Update LeftCommand.java

--- src/main/java/za/wethinkcode/RobotWorlds/commands/LeftCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/LeftCommand.java
@@ -15,7 +15,7 @@ public class LeftCommand extends Command{
 
         target.updateDirection(false);
         String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-        System.out.println(target.getName() + "> " + positionText + " Turned left");
+        System.out.println(target.getName() + " > " + positionText + " Turned left");
         return true;
     }
 

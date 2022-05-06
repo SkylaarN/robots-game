f4047b83201f6f3523bbef7a9cb458a0cd00ac8f
wtc <wtc@pop-os.localdomain>
2022-05-06 08:48:19 +0200

    did border

--- src/main/java/za/wethinkcode/RobotWorlds/Players.java
+++ src/main/java/za/wethinkcode/RobotWorlds/Players.java
@@ -1,19 +1,27 @@
 package za.wethinkcode.RobotWorlds;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Players {
     private static HashMap<String, Robot> robots = new HashMap<String, Robot>();
 
-    static void checkRobot(String name){
+    public static void checkRobot(String name){
         if(!robots.containsKey(name)){
             Robot robot = new Robot(name);
             robots.put(name, robot);
         }
     }
 
-    static Robot getRobot(String name){
+    public static Robot getRobot(String name){
         checkRobot(name);
         return robots.get(name);
     }
+
+//    public static ArrayList<Robot> getRobots(){
+//        ArrayList<SquareObstacle> obstacles = new ArrayList<>();
+//        for (int i = 0; i < robots.size(); i++) {
+//
+//        }
+//    }
 }
--- src/main/java/za/wethinkcode/RobotWorlds/commands/ShootCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/ShootCommand.java
@@ -1,4 +1,31 @@
-//package za.wethinkcode.RobotWorlds.commands;
-//
-//public class ShootCommand {
-//}
+package za.wethinkcode.RobotWorlds.commands;
+
+import edu.princeton.cs.introcs.StdDraw;
+import za.wethinkcode.RobotWorlds.Obstacles;
+import za.wethinkcode.RobotWorlds.Players;
+import za.wethinkcode.RobotWorlds.Position;
+import za.wethinkcode.RobotWorlds.Robot;
+
+public class ShootCommand extends Command{
+    public boolean execute(Robot target){
+        return true;
+    }
+
+//    public boolean hitPlayer(Position a, Position b){
+//        Players.getPlayers();
+//        if(x <= b.getX() && b.getX() <= x + 4 &&
+//                y <= b.getY() && b.getY() <= y + 4){
+//            return true;
+//        }
+//        else{
+//            return false;
+//        }
+//        return true;
+//    }
+
+
+
+    public ShootCommand() {
+        super("shoot");
+    }
+}

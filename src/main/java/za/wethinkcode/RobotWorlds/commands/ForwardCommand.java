f0fc313c859cfacf38830e3ca6ed8c7ccc3b0abe
Gabriel Clarke <gabhilto021@student.wethinkcode.co.za>
2022-05-19 12:50:49 +0000

    Update ForwardCommand.java

--- src/main/java/za/wethinkcode/RobotWorlds/commands/ForwardCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/ForwardCommand.java
@@ -18,26 +18,18 @@ public class ForwardCommand extends Command {
         JSONObject reply = new JSONObject();
         JSONObject data = new JSONObject();
         reply.put("result", "OK");
+
         int nrSteps = Integer.parseInt(getArgument());
         if (target.updatePosition(nrSteps) == Robot.Conditions.SUCCESS){
-            String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-            //target.setStatus(target.getName() + " > " + positionText + " Moved forward by "+nrSteps+" steps.");
             data.put("message", "Done");
-            //data.put("message", target.getName() + " > " + positionText + " Moved forward by "+nrSteps+" steps.");
-
         }
         else if (target.updatePosition(nrSteps) == Robot.Conditions.FAILED_OBSTACLE_DETECTED){
-            String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-            //target.setStatus(target.getName() + " > " + positionText + " Sorry, I have encountered an obstacle.");
             data.put("message", "Obstructed");
-            //data.put("message", target.getName() + " > " + positionText + " Sorry, I have encountered an obstacle.");
         }
         else {
-            String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-            //target.setStatus(target.getName() + " > " + positionText + " Sorry, I cannot go outside my safe zone.");
             data.put("message", "Border");
-            //data.put("message", target.getName() + " > " + positionText + " Sorry, I cannot go outside my safe zone.");
         }
+
         reply.put("data", data);
         target.setStatus(reply.toString());
         return true;

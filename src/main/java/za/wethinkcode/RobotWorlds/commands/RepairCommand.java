4da8f5e7222a50d02890ffa076520e39dae7f185
Gabriel Clarke <gabhilto021@student.wethinkcode.co.za>
2022-05-19 12:55:04 +0000

    Update RepairCommand.java

--- src/main/java/za/wethinkcode/RobotWorlds/commands/RepairCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/RepairCommand.java
@@ -8,24 +8,21 @@ import java.util.concurrent.TimeUnit;
 public class RepairCommand extends Command{
 
     public boolean execute(Robot target){
-//        try {
-//            TimeUnit.SECONDS.sleep(5);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        target.setStatus("repaired");
         JSONObject reply = new JSONObject();
         JSONObject data = new JSONObject();
+
         reply.put("result", "OK");
         data.put("message", "Repairing");
         reply.put("data", data);
+
         target.setStatus(reply.toString());
         target.setStatusType("REPAIR");
+
         target.setHealth(5);
         return true;
     }
 
     public RepairCommand() {
-        super("state");
+        super("repair");
     }
 }

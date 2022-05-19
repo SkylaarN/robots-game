804104b28a5dfda9174fcc0a79e94d902a57a7ea
Thulile Sibiya <tsibiya021@student.wethinkcode.co.za>
2022-05-19 13:43:06 +0000

    Update CommandTest.java

--- src/test/java/za/wethinkcode/Tests/CommandTest.java
+++ src/test/java/za/wethinkcode/Tests/CommandTest.java
@@ -61,17 +61,24 @@ class CommandTest{
         HelpCommand help = new HelpCommand();
         help.execute(robot);
 
-        assertEquals(("< Robot Commands >~" +
-                "OFF  - Shut down robot~" +
-                "HELP - provide information about commands~" +
-                "FORWARD - move forward by specified number of steps, e.g. 'forward 10'~" +
+        String exp = "{\"result\":\"OK\",\"data\":{\"message\":\"< Robot Commands >~OFF  - " +
+                "Shut down robot~HELP - provide information about commands~FORWARD - " +
+                "move forward by specified number of steps, e.g. 'forward 10'~" +
                 "BACK - move backward by specific number of steps, e.g. 'back 10'~" +
-                "RIGHT - turn right by 90 degrees~" +
-                "LEFT - turn left by 90 degrees~" +
-                "ROBOTS - List all robots~" +
-                "SHOOT - Shoot robot~" +
-                "LOOK - See everything in its line of sight~"), robot.getStatus());
+                "RIGHT - turn right by 90 degrees~LEFT - turn left by 90 degrees~" +
+                "ROBOTS - List all robots~SHOOT - Shoot robot~LOOK - See everything in its line of sight~\"}}";
+
+        assertEquals(exp, robot.getStatus());
 
     }
 
+    @Test
+    void TestQuit() {
+        Robot robot = new Robot("Killua");
+        QuitCommand help = new QuitCommand();
+        help.execute(robot);
+
+        assertEquals("Shutting down...", robot.getStatus());
+
+    }
 }

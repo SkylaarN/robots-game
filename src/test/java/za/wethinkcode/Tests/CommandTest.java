c6656c693191aba3f6964ea182532b511d644450
Thulile Sibiya <tsibiya021@student.wethinkcode.co.za>
2022-05-19 12:22:28 +0000

    Update CommandTest.java

--- src/test/java/za/wethinkcode/Tests/CommandTest.java
+++ src/test/java/za/wethinkcode/Tests/CommandTest.java
@@ -55,5 +55,23 @@ class CommandTest{
 
     }
 
+    @Test
+    void TestHelp() {
+        Robot robot = new Robot("Killua");
+        HelpCommand help = new HelpCommand();
+        help.execute(robot);
+
+        assertEquals(("< Robot Commands >~" +
+                "OFF  - Shut down robot~" +
+                "HELP - provide information about commands~" +
+                "FORWARD - move forward by specified number of steps, e.g. 'forward 10'~" +
+                "BACK - move backward by specific number of steps, e.g. 'back 10'~" +
+                "RIGHT - turn right by 90 degrees~" +
+                "LEFT - turn left by 90 degrees~" +
+                "ROBOTS - List all robots~" +
+                "SHOOT - Shoot robot~" +
+                "LOOK - See everything in its line of sight~"), robot.getStatus());
+
+    }
 
 }

e70ed68ac896a0bf4b1b05cfb0e21512557cba01
Gabriel Clarke <gabhilto021@student.wethinkcode.co.za>
2022-05-19 12:54:34 +0000

    Update ReloadCommand.java

--- src/main/java/za/wethinkcode/RobotWorlds/commands/ReloadCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/ReloadCommand.java
@@ -8,24 +8,21 @@ import java.util.concurrent.TimeUnit;
 public class ReloadCommand extends Command{
 
     public boolean execute(Robot target){
-//        try {
-//            TimeUnit.SECONDS.sleep(5);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        target.setStatus("reloaded");
         JSONObject reply = new JSONObject();
         JSONObject data = new JSONObject();
+
         reply.put("result", "OK");
         data.put("message", "Reloading");
         reply.put("data", data);
+
         target.setStatus(reply.toString());
         target.setStatusType("RELOAD");
+
         target.setBullets(8);
         return true;
     }
 
     public ReloadCommand() {
-            super("state");
+            super("reload");
         }
 }

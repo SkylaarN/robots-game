82db244e77dc2edb123b6e895165f1def904dc41
Gabriel Clarke <gabhilto021@student.wethinkcode.co.za>
2022-05-19 12:49:15 +0000

    Update BackCommand.java

--- src/main/java/za/wethinkcode/RobotWorlds/commands/BackCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/BackCommand.java
@@ -18,25 +18,18 @@ public class BackCommand extends Command {
         JSONObject reply = new JSONObject();
         JSONObject data = new JSONObject();
         reply.put("result", "OK");
+
         int nrSteps = Integer.parseInt(getArgument());
         if (target.updatePosition(-1 * nrSteps) == Robot.Conditions.SUCCESS){
-            String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-            //target.setStatus(target.getName() + " > " + positionText + " Moved back by "+nrSteps+" steps.");
             data.put("message", "Done");
-            //data.put("massage", target.getName() + " > " + positionText + " Moved back by "+nrSteps+" steps.");
         }
         else if (target.updatePosition(nrSteps) == Robot.Conditions.FAILED_OBSTACLE_DETECTED){
-            String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-            //target.setStatus(target.getName() + " > " + positionText + " Sorry, I have encountered an obstacle.");
             data.put("message", "Obstructed");
-            //data.put("massage", target.getName() + " > " + positionText + " Sorry, I have encountered an obstacle.");
         }
         else {
-            String positionText = "[" + target.getPosition().getX() + "," + target.getPosition().getY() + "]";
-            //target.setStatus(target.getName() + " > " + positionText + " Sorry, I cannot go outside my safe zone.");
             data.put("message", "Border");
-            //data.put("massage", target.getName() + " > " + positionText + " Sorry, I cannot go outside my safe zone.");
         }
+
         reply.put("data", data);
         target.setStatus(reply.toString());
         return true;

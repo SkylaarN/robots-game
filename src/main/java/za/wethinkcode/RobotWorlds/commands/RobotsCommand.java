d35b94917998eeabf8947ba7030640d997c1fa5e
Gabriel Clarke <gabhilto021@student.wethinkcode.co.za>
2022-05-19 12:56:30 +0000

    Update RobotsCommand.java

--- src/main/java/za/wethinkcode/RobotWorlds/commands/RobotsCommand.java
+++ src/main/java/za/wethinkcode/RobotWorlds/commands/RobotsCommand.java
@@ -1,13 +1,19 @@
 package za.wethinkcode.RobotWorlds.commands;
 
+import org.json.JSONObject;
 import za.wethinkcode.RobotWorlds.Robot;
 import za.wethinkcode.RobotWorlds.SimpleServer;
 
 public class RobotsCommand extends Command{
     public boolean execute(Robot target) {
+        JSONObject reply = new JSONObject();
+        JSONObject data = new JSONObject();
 
-        target.setStatus(
-                "Robots: " + SimpleServer.listRobots);
+        reply.put("result", "OK");
+        data.put("message", "Robots: " + SimpleServer.listRobots);
+        reply.put("data", data);
+
+        target.setStatus(reply.toString());
         return true;
     }
 

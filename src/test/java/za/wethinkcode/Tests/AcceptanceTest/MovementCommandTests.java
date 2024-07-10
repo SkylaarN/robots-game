7d1da5084b6619bc538db36a69b0413ecb00a7a7
kdhlamini023b <kdhlamini023b@student.wethinkcode.co.za>
2024-07-10 16:00:59 +0200

    vaild forward command test_1 passed against ref

--- src/test/java/za/wethinkcode/Tests/AcceptanceTest/MovementCommandTests.java
+++ src/test/java/za/wethinkcode/Tests/AcceptanceTest/MovementCommandTests.java
@@ -1,14 +1,17 @@
 package za.wethinkcode.Tests.AcceptanceTest;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import org.junit.jupiter.api.Test;
 import za.wethinkcode.RobotWorlds.RobotWorldClient;
 import za.wethinkcode.RobotWorlds.RobotWorldJsonClient;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 
-    public class MovementCommandTests {
+public class MovementCommandTests {
         /**
 
          * I want to launch my robot in the online robot world
@@ -29,5 +32,33 @@ import za.wethinkcode.RobotWorlds.RobotWorldJsonClient;
         void disconnectFromServer(){
             serverClient.disconnect();
         }
+
+    @Test
+    void ValidForwardCommand() {
+        // Given that I am connected to a running Robot Worlds server
+        // And the world is of size 1x1 (The world is configured or hardcoded to this size)
+        assertTrue(serverClient.isConnected());
+
+        // When I send a valid launch request to the server
+        String requestLaunch = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+        JsonNode responseLaunch = serverClient.sendRequest(requestLaunch);
+
+        String request = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"forward\"," +
+                "  \"arguments\": [0]" +
+                "}";
+        JsonNode response = serverClient.sendRequest(request);
+
+        // response from the server for vaild commmand
+        assertNotNull(response.get("result"));
+        assertEquals("OK", response.get("result").asText());
+
+    }
+
 
 }

fe990cfbaeea50239070b81fcfd8170278b52c46
kdhlamini023b <kdhlamini023b@student.wethinkcode.co.za>
2024-07-11 10:36:44 +0200

    invaild and valid command test added

--- src/test/java/za/wethinkcode/Tests/AcceptanceTest/MovementCommandTests.java
+++ src/test/java/za/wethinkcode/Tests/AcceptanceTest/MovementCommandTests.java
@@ -67,8 +67,8 @@ public class MovementCommandTests {
 //            launching robot
         String requestLaunch = "{" +
                 "  \"robot\": \"HAL\"," +
-                "  \"command\": \"forward\"," +
-                "  \"arguments\": [0]" +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
                 "}";
 //        get response from server after launching
         JsonNode response = serverClient.sendRequest(requestLaunch);
@@ -119,8 +119,8 @@ public class MovementCommandTests {
 //            launching robot
         String requestLaunch = "{" +
                 "  \"robot\": \"HAL\"," +
-                "  \"command\": \"forward\"," +
-                "  \"arguments\": [0]" +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
                 "}";
 //        get response from server after launching
         JsonNode response = serverClient.sendRequest(requestLaunch);
@@ -137,6 +137,58 @@ public class MovementCommandTests {
         assertNotNull(response_forward.get("result").asText());
         assertEquals("ERROR",response_forward.get("result").asText());
 
+    }
+
+    @Test
+    void InvaildLeftCommand(){
+//            checks if connection is made
+        assertTrue(serverClient.isConnected());
+//            launching robot
+        String requestLaunch = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"forward\"," +
+                "  \"arguments\": [0]" +
+                "}";
+//        get response from server after launching
+        JsonNode response = serverClient.sendRequest(requestLaunch);
+
+//        place invalid left command
+        String request = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"left 10\"," +
+                "  \"arguments\": [Left]" +
+                "}";
+        JsonNode response_forward = serverClient.sendRequest(request);
+
+//        running error for a invaild left command returns "ERROR"
+        assertNotNull(response_forward.get("result").asText());
+        assertEquals("ERROR",response_forward.get("result").asText());
+    }
+
+    @Test
+    void ValidLeftCommand() {
+        // Given that I am connected to a running Robot Worlds server
+        // And the world is of size 1x1 (The world is configured or hardcoded to this size)
+        assertTrue(serverClient.isConnected());
+
+        // When I send a valid launch request to the server
+        String requestLaunch = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+        JsonNode responseLaunch = serverClient.sendRequest(requestLaunch);
+//        request for back command
+        String request = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"turn\"," +
+                "  \"arguments\": [Left]" +
+                "}";
+        JsonNode response = serverClient.sendRequest(request);
+
+        // response from the server for vaild commmand
+        assertNotNull(response.get("result"));
+        assertEquals("OK", response.get("result").asText());
     }
 
 

e984bcc8092fed64985815fe9a737ae2a5c393bc
mralokoana023b <mralokoana023b@student.wethinkcode.co.za>
2024-07-29 14:27:28 +0200

    Testing pipeline

--- src/main/java/za/wethinkcode/RobotWorlds/Obstacles.java
+++ src/main/java/za/wethinkcode/RobotWorlds/Obstacles.java
@@ -9,6 +9,7 @@ public class Obstacles {
     public static ArrayList<SquareObstacle> obstacles = new ArrayList<>();
     public static Random random = new Random();
 
+    //trying
     public static void generateObstacles(){
         for (int i = 0; i < 100; i++) {
             int x = (random.nextInt(60)- 30) * 5;
